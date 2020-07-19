e9d907c62662b7de5f05a47dfaab21cb98775616
Alley Purcell <zanastar@gmail.com>
2020-07-19 18:04:18 -0500

    React Component Definition (Advanced)

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ const list = [
   },
 ];
 
-function App() {
-
-  return (
+const App = () => (
     <div>
       <h1>My Hacker Stories</h1>
 
@@ -31,24 +29,19 @@ function App() {
       <hr/>
 
       <List />
-      <List />
     </div>
-  );
-}
+);
 
-function List() {
-  return list.map(function(item) {
-    return(
-      <div key={item.objectID}>
-        <span>
-          <a href="{item.url}">{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    );
-  });
-}
+const List = () =>
+  list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
 
 export default App;
