2368a359c18bab5fdf89d951482f0a0e884036b7
Alley Purcell <zanastar@gmail.com>
2020-07-19 23:00:47 -0500

    React Asynchronous Data

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ const initialStories =[
   },
 ];
 
+const getAsyncStories = () =>
+  new Promise(resolve =>
+    setTimeout(
+    () => resolve({ data: { stories: initialStories } }),
+    2000
+  )
+);
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -36,7 +43,13 @@ const App = () => {
     'React'
   );
 
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = item => {
     const newStories = stories.filter(
