a1be902f1246a29f5c667a7ec28da4ca5b15a88f
Alley Purcell <zanastar@gmail.com>
2020-07-19 20:38:30 -0500

    React Side-Effects

--- src/App.js
+++ src/App.js
@@ -20,8 +20,14 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
-  
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') ||'React'
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
+
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
